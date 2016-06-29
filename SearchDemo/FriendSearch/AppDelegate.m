7db52e1bb10ddf7393598a731c06fb359028308f
chengxuyuan1 <1271253083@qq.com>
2016-06-29 13:17:55 +0800

    00
    00

--- SearchDemo/FriendSearch/AppDelegate.m
+++ SearchDemo/FriendSearch/AppDelegate.m
@@ -39,6 +39,7 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    //00000
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
